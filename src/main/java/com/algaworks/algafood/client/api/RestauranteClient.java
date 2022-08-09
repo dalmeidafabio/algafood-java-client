05720fe5ed6005d7d6f51d1dd08457eaeb5dfa54
Fábio D'Almeida <fabio.oliveira.cc@gmail.com>
2022-08-09 15:07:39 -0300

    16.12. Desafio: Implementando uma requisição POST no client Java
    16.12. Desafio: Implementando uma requisição POST no client Java

--- src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
+++ src/main/java/com/algaworks/algafood/client/api/RestauranteClient.java
@@ -4,17 +4,20 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
+import com.algaworks.algafood.client.model.RestauranteModel;
 import com.algaworks.algafood.client.model.RestauranteResumoModel;
+import com.algaworks.algafood.client.model.input.RestauranteInput;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class RestauranteClient {
 
-	private static final String RESOURCE_PATH = "/restauranteSSSs";
+	private static final String RESOURCE_PATH = "/restaurantes";
 	
 	private RestTemplate restTemplate;
 	private String url;
@@ -30,6 +33,18 @@ public class RestauranteClient {
 			
 		} catch (RestClientResponseException e) {
 			throw new ClientApiException(e.getMessage(), e);
+		}
+	}
+	
+	public RestauranteModel adicionar(RestauranteInput restaurante) {
+		try {
+			URI resourceUri = URI.create(url + RESOURCE_PATH);
+			
+			return restTemplate
+					.postForObject(resourceUri, restaurante, RestauranteModel.class);
+			
+		} catch(HttpClientErrorException e) {
+		    throw new ClientApiException(e.getMessage(), e);
 		}
 	}
 }
